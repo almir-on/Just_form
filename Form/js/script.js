557d81ad44cf9e89dda251fb48d18e569025efd9
Alesia <84683953+almir-on@users.noreply.github.com>
2022-08-02 11:53:53 +0300

    Just form

---
+++ Form/js/script.js
@@ -0,0 +1,44 @@
+// ИСПОЛЬЗУЕТСЯ РЕДКО
+let chooseBtn = document.getElementById('choose'), 
+   receiveBtn = document.getElementById('receive'), 
+   heading = document.getElementsByTagName('h2')[0], 
+   nameInput = document.getElementsByClassName('contactform_name') [0], 
+   text = document.getElementsByName('message')[0];
+
+
+// БОЛЕЕ НОВЫЕ СПОСОБЫ ПОЛУЧЕНИЕ ЭЕЛЕМНТА
+
+let phoneInput = document.querySelector('.contactform_phone'), 
+   mailInput = document.querySelectorAll('.contactform_mail')[0], 
+   modal = document.querySelector('.modal'),
+   closeBtn = document.querySelector('.close');
+   
+
+
+
+function hover() {
+   heading.textContent = 'Куда полетим?';
+};
+
+
+chooseBtn.addEventListener('mouseenter', function() {
+   heading.textContent = 'Куда полетим?';
+});  
+
+
+
+chooseBtn.addEventListener('mouseleave', function() {
+   heading.textContent = 'Всё включено!';
+}); 
+
+receiveBtn.addEventListener('click', function(){ 
+   modal.style.display = 'block';                
+});
+
+closeBtn.addEventListener('click', function(){ 
+   modal.style.display = 'none';                
+});
+
+nameInput.addEventListener('input', function(){
+   text.value = `Меня зовут ${nameInput.value}. И я хочу спросить:`  
+});
